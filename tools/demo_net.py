5dc677e54db348a25e86932f38f0f83981d1e0a8
Binh Pham <tnghia14111998@gmail.com>
2020-06-01 11:24:41 +0700

    Fix bug

--- tools/demo_net.py
+++ tools/demo_net.py
@@ -71,7 +71,7 @@ class VideoWriter(object):
 
     def __init__(self, cfg, out_width=0, out_height=0):
         # self._fourcc = VideoWriter_fourcc('M', 'J', 'P', 'G')
-        self._fourcc = VideoWriter_fourcc('M', 'P', '4', 'V')
+        self._fourcc = cv2.VideoWriter_fourcc('M', 'P', '4', 'V')
         if out_width > 0 and out_height > 0:
             self._out = cv2.VideoWriter(
                 cfg.DEMO.OUT_VIDEO, self._fourcc, cfg.DEMO.OUT_FRAME_RATE, (out_width, out_height))
