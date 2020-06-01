39eac0268c819ced6d2d61f40bafa4516ade475f
Binh Pham <tnghia14111998@gmail.com>
2020-06-01 11:11:42 +0700

    Change type video from avi to mp4

--- tools/demo_net.py
+++ tools/demo_net.py
@@ -70,15 +70,17 @@ class VideoReader(object):
 class VideoWriter(object):
 
     def __init__(self, cfg, out_width=0, out_height=0):
+        # self._fourcc = VideoWriter_fourcc('M', 'J', 'P', 'G')
+        self._fourcc = VideoWriter_fourcc('M', 'P', '4', 'V')
         if out_width > 0 and out_height > 0:
-            self.out = cv2.VideoWriter(cfg.DEMO.OUT_VIDEO, cv2.VideoWriter_fourcc(
-                'M', 'J', 'P', 'G'), cfg.DEMO.OUT_FRAME_RATE, (out_width, out_height))
+            self._out = cv2.VideoWriter(
+                cfg.DEMO.OUT_VIDEO, self._fourcc, cfg.DEMO.OUT_FRAME_RATE, (out_width, out_height))
         else:
-            self.out = cv2.VideoWriter(cfg.DEMO.OUT_VIDEO, cv2.VideoWriter_fourcc(
-                'M', 'J', 'P', 'G'), cfg.DEMO.OUT_FRAME_RATE, (cfg.DEMO.OUT_FRAME_WIDTH, cfg.DEMO.OUT_FRAME_HEIGHT))
+            self._out = cv2.VideoWriter(cfg.DEMO.OUT_VIDEO, self._fourcc, cfg.DEMO.OUT_FRAME_RATE, (
+                cfg.DEMO.OUT_FRAME_WIDTH, cfg.DEMO.OUT_FRAME_HEIGHT))
 
     def write(self, frame):
-        self.out.write(frame)
+        self._out.write(frame)
 
 
 def demo(cfg):
